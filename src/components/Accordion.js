e2c25bc86b32adfafeb868a5005d0b96abc8e729
Kristoffer <hello@kristoffer.is>
2018-07-18 10:10:33 +0200

    Cleanup

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,15 @@
 // @flow
 
-import React from 'react'
-import type { Node } from 'react'
+import React, { type Node } from 'react'
 
-type Props = {
+export const Accordion = ({
+  children,
+  className,
+  ...props
+}: {
   children: Node,
   className?: string,
-}
-
-export const Accordion = ({ children, className, ...props }: Props) => (
+}) => (
   <dl className={className} {...props} role="presentation">
     {children}
   </dl>
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,14 +1,15 @@
 // @flow
 
-import React from 'react'
-import type { Node } from 'react'
+import React, { type Node } from 'react'
 
-type Props = {
+export const Panel = ({
+  children,
+  className,
+  ...props
+}: {
   children: Node,
   className?: string,
-}
-
-export const Panel = ({ children, className, ...props }: Props) => (
+}) => (
   <dd className={className} {...props}>
     {children}
   </dd>
